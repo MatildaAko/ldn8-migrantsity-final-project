72c978c39e756df483d5ec8710626393a22d188b
Davood Khoshnood <khoshnood1977@gmail.com>
2022-07-06 14:57:55 +0100

    Tables services Week1

--- server/api.js
+++ server/api.js
@@ -13,7 +13,7 @@ router.get("/", (_, res) => {
 //Applicants
 router.get("/applicants", (req, res) => {
 	const queryString = `
-		Select applicants.*, username, email, reg_date, user_types.user_type, genders.gender,
+		Select applicants.*, genders.gender,
  			sex_orientations.sex_orientation, cities.city, age_bands.age_band, 
 			ethnic_groups.ethnic_group, religions.religion
 		From applicants
@@ -35,7 +35,7 @@ router.get("/applicants/:applicantId", (req, res) => {
 	const params = [applicantId];
 	const selectQuery = "Select * From applicants Where id = $1";
 	const queryString = `
-		Select applicants.*, username, email, reg_date, user_types.user_type, genders.gender,
+		Select applicants.*, genders.gender,
  			sex_orientations.sex_orientation, cities.city, age_bands.age_band, 
 			ethnic_groups.ethnic_group, religions.religion
 		From applicants
@@ -48,7 +48,6 @@ router.get("/applicants/:applicantId", (req, res) => {
 		Inner join ethnic_groups on ethnic_groups.id = ethnic_group_id
 		Inner join religions on religions.id = religion_id
 		Where applicants.id = $1`;
-console.log(applicantId, selectQuery, queryString);
 	pool.query(selectQuery, params)
 	.then((result) => {
 		if (result.rows == 0) {
@@ -66,11 +65,19 @@ router.post("/applicants", (req, res) => {
 	const values = [];
 	const fields = [];
 
+	// const username = `${req.body.first_name}-${req.body.surname}`;
+
 	Object.keys(req.body).map((param, index) => {
     values.push(`$${index + 1}`);
     fields.push(param);
     params.push(req.body[param]);
 	});
+
+// Extra line to don't use users---------------------
+	values.push(`$${Object.keys(req.body).length+1}`);
+	fields.push("user_id");
+    params.push(1);
+//---------------------------------------------------
 
 	const queryString = `Insert Into applicants (${fields.join(",")}) 
   					   		Values (${values.join(",")})`;
@@ -99,7 +106,7 @@ router.put("/applicants/:applicantId", (req, res) => {
 			res.status(400).send("Applicant doesn't exist!");
 		} else {
 			pool.query(queryString, params)
-			.then(() => res.status(201).send("Applicant created."))
+			.then(() => res.status(201).send("Applicant has been updated."))
 			.catch((error) => res.status(500).json(error));
 		}
 	}).catch((error) => res.status(500).json(error));
@@ -148,11 +155,75 @@ router.get("/jobs", (req, res) => {
 	.catch((error) => res.status(500).json(error));
 });
 
+//Genders
+router.get("/genders", (req, res) => {
+	const queryString = "Select * From genders";
+	pool.query(queryString)
+	.then((result) => res.status(201).json(result.rows))
+	.catch((error) => res.status(500).json(error));
+});
+
+//Sex Orientations
+router.get("/sex_orientations", (req, res) => {
+	const queryString = "Select * From sex_orientations";
+	pool.query(queryString)
+	.then((result) => res.status(201).json(result.rows))
+	.catch((error) => res.status(500).json(error));
+});
+
+//Age Bands
+router.get("/age_bands", (req, res) => {
+	const queryString = "Select * From age_bands";
+	pool.query(queryString)
+	.then((result) => res.status(201).json(result.rows))
+	.catch((error) => res.status(500).json(error));
+});
+
+//Flexible Working
+router.get("/flexible_working", (req, res) => {
+	const queryString = "Select * From flexible_working";
+	pool.query(queryString)
+	.then((result) => res.status(201).json(result.rows))
+	.catch((error) => res.status(500).json(error));
+});
+
+//Working Pattern
+router.get("/working_pattern", (req, res) => {
+	const queryString = "Select * From working_pattern";
+	pool.query(queryString)
+	.then((result) => res.status(201).json(result.rows))
+	.catch((error) => res.status(500).json(error));
+});
+
+//Religions
+router.get("/religions", (req, res) => {
+	const queryString = "Select * From religions";
+	pool.query(queryString)
+	.then((result) => res.status(201).json(result.rows))
+	.catch((error) => res.status(500).json(error));
+});
+
+//Ethnic Groups
+router.get("/ethnic_groups", (req, res) => {
+	const queryString = "Select * From ethnic_groups";
+	pool.query(queryString)
+	.then((result) => res.status(201).json(result.rows))
+	.catch((error) => res.status(500).json(error));
+});
+
+//Cities
+router.get("/cities", (req, res) => {
+	const queryString = "Select * From cities";
+	pool.query(queryString)
+	.then((result) => res.status(201).json(result.rows))
+	.catch((error) => res.status(500).json(error));
+});
+
 //Applications
 router.get("/applications/:applicationId", (req, res) => {
-	const id = req.params.applicationId;
+	const applicationId = req.params.applicationId;
 	const queryString = "Select * From applications where id = $1";
-	pool.query(queryString, [id])
+	pool.query(queryString, [applicationId])
 	.then((result) => res.status(201).json(result.rows))
 	.catch((error) => res.status(500).json(error));
 });
@@ -165,11 +236,83 @@ router.get("/applications", (req, res) => {
 });
 
 router.get("/:applicantId/applications", (req, res) => {
-	const id = req.params.applicantId;
+	const applicationId = req.params.applicantId;
 	const queryString = "Select * From applications where applicant_id = $1";
-	pool.query(queryString, [id])
+	pool.query(queryString, [applicationId])
 	.then((result) => res.status(201).json(result.rows))
 	.catch((error) => res.status(500).json(error));
 });
+
+//Education
+router.get("/:applicantId/education", (req, res) => {
+	const applicantId = req.params.applicantId;
+	const queryString = "Select * From education Where applicant_id = $1";
+	pool.query(queryString, [applicantId])
+	.then((result) => res.status(201).json(result.rows))
+	.catch((error) => res.status(500).json(error));
+});
+
+router.post("/education", (req, res) => {
+	const params = [];
+	const values = [];
+	const fields = [];
+
+	Object.keys(req.body).map((param, index) => {
+		values.push(`$${index + 1}`);
+		fields.push(param);
+		params.push(req.body[param]);
+	});
+
+	const queryString = `Insert Into education (${fields.join(",")}) 
+  					   		Values (${values.join(",")})`;
+	pool.query(queryString, params)
+	.then(() => res.status(201).send("Education added."))
+	.catch((error) => res.status(500).json(error));
+});
+
+router.put("/education/:educationId", (req, res) => {
+	const educationId = req.params.educationId;
+	const insideParams = [];
+	const params = [educationId];
+
+	Object.keys(req.body).map((param, index) => {
+    insideParams.push(`${param} = $${index + 2}`);
+    params.push(req.body[param]);
+	});
+
+	const selectQuery = "Select * From education Where id = $1";
+	const queryString = `Update education Set ${insideParams.join(",")} 
+  					   		Where id = $1`;
+
+	pool.query(selectQuery, [educationId])
+	.then((result) => {
+		if (result.rows == 0) {
+			res.status(400).send("Education doesn't exist!");
+		} else {
+			pool.query(queryString, params)
+			.then(() => res.status(201).send("Education has been updated."))
+			.catch((error) => res.status(500).json(error));
+		}
+	}).catch((error) => res.status(500).json(error));
+});
+
+router.delete("/education/:educationId", (req, res) => {
+	const educationId = req.params.educationId;
+	const selectQuery = "Select * From education Where id = $1";
+	const queryString = "Delete From education Where id = $1";
+	const params = [educationId];
+	pool.query(selectQuery, [educationId])
+	.then((result) => {
+		if (result.rows == 0) {
+			res.status(400).send("Education doesn't exist!");
+		} else {
+			pool.query(queryString, params)
+			.then(() => res.status(201).send("Education has been deleted."))
+			.catch((error) => res.status(500).json(error));
+		}
+	}).catch((error) => res.status(500).json(error));
+
+});
+
 
 export default router;
