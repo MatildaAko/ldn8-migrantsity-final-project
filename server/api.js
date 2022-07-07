01e030a6139a4cfcf19d06a14e3b825c77d370f4
Davood Khoshnood <khoshnood1977@gmail.com>
2022-07-07 23:35:02 +0100

    Change query for Applicants

--- server/api.js
+++ server/api.js
@@ -112,7 +112,7 @@ const applicantsQueryString = `
 
 const applicationsQueryString = `
 	Select * from (Select applications.id as id, applicant_id, first_name, surname, email, 
-			right_to_work, skills, gap_reasons, job_id, 
+			currently_work, right_to_work, skills, gap_reasons, job_id, 
 			jobs.title as job_title, jobs.description as job_description, 
 			skills_require, cover_letter, applications.description, status_id, status
 	From applications 
