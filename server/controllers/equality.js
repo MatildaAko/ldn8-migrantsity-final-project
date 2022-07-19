5c7973057905bd4b665934f94cb3fde1996e6d0a
Davood Khoshnood <khoshnood1977@gmail.com>
2022-07-19 17:13:54 +0100

    new change

--- server/controllers/equality.js
+++ server/controllers/equality.js
@@ -10,7 +10,17 @@ const getReligions = (req, res) => generalController.get(req, res);
 const getEthnicGroups = (req, res) => generalController.get(req, res);
 const getEquality = (req, res) => {
     const queryString = `
-   ${generalController.applicantsQueryString} `;
+   select * from (
+   Select 'cities' as tableName, city as title, country as groupName from cities union 
+   Select 'age_bands' , age_band , '' from age_bands union
+   Select 'sex_orientations' , sex_orientation , '' from sex_orientations union 
+   Select 'genders' , gender , '' from genders union
+   Select 'working_pattern' , pattern , '' from working_pattern union
+   Select 'flexible_working' , flexible, '' from flexible_working union
+   Select 'religions' , religion, '' from religions union
+   Select 'ethnic_groups' , ethnic_group , '' from ethnic_groups 
+   ) AllEqualities order by tableName, groupName, title   
+   `;
 	console.log(queryString);
     pool.query(queryString)
 	.then((result) => res.status(201).json(result.rows))
